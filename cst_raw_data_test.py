6024b48bcbaf45821e9551e4e4d008e0d3b2e13b
Adriana Terman <terman.adriana@gmail.com>
2023-05-22 15:48:29 +0200

    raw signal - solved, but disconnections do not work

--- cst_raw_data_test.py
+++ cst_raw_data_test.py
@@ -1,21 +1,29 @@
 import streamlit as st
 import template.constant as constant
-import template.test as test
 import requests
 import json
 from pylife.api_functions import map_data
 from pylife.api_functions import map_data_filt
 from pylife.api_functions import get_sig_info
    
+
+st.session_state.url_data = "https://preprod.chronolife.net/api/2/data"
+st.session_state.api_key = '3-1krbPQoufnmNVN6semRA'
+st.session_state.end_user = "4vk5VJ"
+st.session_state.date = "2023-05-17"
+st.session_state.start_time = "14:00"
+st.session_state.end_time = "14:05"
+
 def get_raw_data():
     
-    url         = st.session_state.url_data
-    api_key     = st.session_state.api_key
-    user        = st.session_state.end_user
+    raw_data    = []
+    url         = "https://preprod.chronolife.net/api/2/data"
+    api_key     = '3-1krbPQoufnmNVN6semRA'
+    user        = "4vk5VJ"
     types       = constant.TYPE()["SIGNALS"]
-    date        = st.session_state.date
-    time_gte    = st.session_state.start_time + ":00"
-    time_lt     = st.session_state.end_time + ":00"
+    date        = "2023-05-17"
+    time_gte    = "14:00:00"
+    time_lt     = "14:05:00"
     
     params = {
            'user':      user, # sub-user username
@@ -27,12 +35,33 @@ def get_raw_data():
     
     # Perform the POST request authenticated with YOUR API key (NOT the one of the sub-user!).
     reply = get_reply(params, url, api_key)
-    message, status_code = test.api_status(reply)
+    message, status_code = api_status(reply)
 
     raw_data = request_datas(reply, status_code)
 
     # Declare as global variable
-    st.session_state.smart_textile_raw_data = raw_data
+    return raw_data
+
+def api_status(reply, user_text='Username'):
+    
+    status_code = reply.status_code
+        
+    if status_code == 200:
+        message = "api_auth_200" #'Connected'
+    elif status_code == 400:
+        message = "api_auth_400" #'Part of the request could not be parsed or is incorrect'
+    elif status_code == 401:
+        message = "api_auth_401" #'Incorrect API key'
+    elif status_code == 403:
+        message = "api_auth_403" #'Not authorized'
+    elif status_code == 404:
+        message = "api_auth_404" #'Incorrect url'
+    elif status_code == 500:
+        message = "api_auth_500"
+    elif status_code == 0:
+        message = "api_auth_0" #"You are disconnect"
+        
+    return message, status_code
 
 def get_reply(params, url, api_key):
     reply = requests.get(url, headers={"X-API-Key": api_key}, params=params)
@@ -40,7 +69,6 @@ def get_reply(params, url, api_key):
 
 def request_datas(reply, status_code):
     datas = []
-    raw_data    = []
 
     if status_code == 200:  
         json_list_of_records = json.loads(reply.text) 
@@ -79,6 +107,11 @@ def map_filtered_data(datas, raw_data):
         for val_type in ["times", "sig"]:
             tmp = get_sig_info(datas_filtered_mapped, key_type, verbose=0)
             raw_data[key_type][val_type] = tmp[val_type]
+
+# -- test get_raw_data -- 
+
+
+raw_data = get_raw_data()
 
 
 
