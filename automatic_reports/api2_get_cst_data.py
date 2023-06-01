update for Ludo bug (when no garmin data) and update garmin cardio rate calculation -> value per minute

automatic_reports/api2_get_cst_data.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 
-from automatic_reports.useful_functions import find_time_intervals, sum_time_intervals, timedelta_formatter, unwrap
+from automatic_reports.useful_functions import find_time_intervals, sum_time_intervals, timedelta_formatter, unwrap, unwrap_ratio_inspi_expi
 from automatic_reports.config import CST_SIGNAL_TYPES
 from automatic_reports.config import RED_ALERT, GREEN_ALERT, ALERT_SIZE, ACTIVITY_THREASHOLD
 from automatic_reports.config import TACHYPNEA_TH, BRADYPNEA_TH, TACHYCARDIA_TH, BRADYCARDIA_TH,\
@@ -101,40 +101,40 @@ def error_management(date, reply) :
 
 def initialize_dictionary_with_template() -> dict :  
     activity_dict = {
-        'steps' : None,
-        'averaged_activity' : None,
-        'distance' : None,
+        'steps' : "",
+        'averaged_activity' : "",
+        'distance' : "",
         }
     anomalies_dict = initialize_alerts_with_template()
     breath_dict = {
-        'rate' : None, 
-        'rate_var' : None,
-        'inspi_expi' : None,
+        'rate' : "", 
+        'rate_var' : "",
+        'inspi_expi' : "",
         }
     cardio_dict = {
-        'rate' : None, 
-        'rate_var' : None,
+        'rate' : "", 
+        'rate_var' : "",
          }
     duration_dict = {
-        'intervals' : None, 
-        'collected' : None,
-        'day' : None, 
-        'night' : None,
-        'rest' : None,
-        'active' : None,
+        'intervals' : "", 
+        'collected' : "",
+        'day' : "", 
+        'night' : "",
+        'rest' : "",
+        'active' : "",
         }
     temperature_dict = {
-        'right' : None,
-        'left' : None,
-        'mean_left' : None, 
-        'mean_right' : None, 
-        'min_left' : None, 
-        'min_right' : None, 
-        'max_left' : None, 
-        'max_right' : None, 
+        'right' : "",
+        'left' : "",
+        'mean_left' : "", 
+        'mean_right' : "", 
+        'min_left' : "", 
+        'min_right' : "", 
+        'max_left' : "", 
+        'max_right' : "", 
     }
     dict_template = {
-                    'user_id' : None,
+                    'user_id' : "",
                     'activity': copy.deepcopy(activity_dict),
                     'anomalies': copy.deepcopy(anomalies_dict),
                     'breath': copy.deepcopy(breath_dict),
@@ -147,8 +147,8 @@ def initialize_dictionary_with_template() -> dict :
 def initialize_alerts_with_template() -> dict :
     pdf_info = {
         "path" : GREEN_ALERT,
-        "x" : None,
-        "y" : None,
+        "x" : "",
+        "y" : "",
         "w" : ALERT_SIZE,
         "h" : ALERT_SIZE,
         "exists" : False,
@@ -159,7 +159,7 @@ def initialize_alerts_with_template() -> dict :
         "resting" : "",
         "percentage" : "",
         "duration" : "",
-        "values" : []
+        "values" : ""
         }
 
     dict_template = {
@@ -174,7 +174,7 @@ def initialize_alerts_with_template() -> dict :
 def add_cardio(date, datas, cardio_dict):
     rate = get_cst_result_info(date, datas, result_type='heartbeat')
     rate_var = get_cst_result_info(date, datas, result_type='HRV')
-    qt = get_cst_result_info_segment(date, datas, result_type='qt_c_framingham_per_seg')
+    qt = get_cst_result_info_segment(date, datas, result_type='qt_c_framingham_per_seg', type = 'qt')
 
     cardio_dict['rate'] = rate
     cardio_dict['rate_var'] = rate_var
@@ -183,7 +183,7 @@ def add_cardio(date, datas, cardio_dict):
 def add_breath(date, datas, breath_dict):
     rate = get_cst_result_info(date, datas, result_type='breath_2_brpm')
     rate_var = get_cst_result_info(date, datas, result_type='breath_2_brv')
-    inspi_expi = get_cst_result_info_segment(date, datas, result_type='breath_2_inspi_over_expi') # TO CHANGE!!!
+    inspi_expi = get_cst_result_info_segment(date, datas, result_type='breath_2_inspi_over_expi', type = 'ratio')
 
     breath_dict['rate'] = rate
     breath_dict['rate_var'] = rate_var
@@ -349,11 +349,12 @@ def add_anomalies(results_dict):
     if len(values) > 0:
         value_max = round(np.quantile(values, 0.9))
         value_min = round(np.quantile(values, 0.1))
+        value_mean =  round(np.mean(values))
         alerts_dict["qt"]["values"] = values
         alerts_dict["qt"]["min"] = value_min
         alerts_dict["qt"]["max"] = value_max
-        alerts_dict["qt"]["mean"] = round(np.mean(values))
-        if(value_max > QT_MAX_TH or value_min < QT_MIN_TH):
+        alerts_dict["qt"]["mean"] = value_mean
+        if(value_mean > QT_MAX_TH or value_mean < QT_MIN_TH):
             alerts_dict["qt"]["path"]  = RED_ALERT
             alerts_dict["qt"]["exists"] = True
 
@@ -399,7 +400,7 @@ def get_cst_result_info(date, datas, result_type):
 
     return output
 
-def get_cst_result_info_segment(date, datas, result_type):
+def get_cst_result_info_segment(date, datas, result_type, type):
     times = []
     values = []
     output = pd.DataFrame({
@@ -411,7 +412,11 @@ def get_cst_result_info_segment(date, datas, result_type):
         if result_type == data['type']:
             timestamp = get_timestamp(data['_id'])
             times.append(timestamp)
-            segment_values = unwrap(data['values'])
+            if type == "ratio" : 
+                segment_values = unwrap_ratio_inspi_expi(data['values'])
+            else :
+                segment_values = unwrap(data['values'])
+
             if np.size(segment_values) > 1:         # Nan values have size = 1       
                 mean_value = round(np.mean(segment_values))
             else: mean_value = math.nan
@@ -459,8 +464,13 @@ def get_timestamp(id_:str):
 # # user_id = "5Nwwut" 
 # # date = "2023-05-04" 
 # # -- Adriana
-# user_id = "6o2Fzp"
-# date = "2023-05-24"
+
+# # user_id = "6o2Fzp"
+# # date = "2023-05-24"
+# # Ludo
+# user_id = "4vk5VJ"
+# date = "2023-05-25" 
+
 
 # if prod == True :
 #     api = API_KEY_PROD

automatic_reports/api2_get_garmin_data_old.py
@@ -68,29 +68,27 @@ def save_datas_in_dict(date, user_id, datas, api, url) -> dict :
     results_dict = initialize_dictionary_with_template()
     # User id
     results_dict['user_id']  = user_id
+    # Activity 
+    add_activity(datas, results_dict['activity'])
+    # Body battery
+    add_body_battery(datas, results_dict["body_battery"])
+    # Breath
+    add_breath(datas, results_dict['breath'])
+    # Calories 
+    add_calories(datas, results_dict['calories'])
+    # Cardio 
+    add_cardio(datas, results_dict['cardio'])
+    # Compute durations
+    add_durations(date, results_dict)
+    # Intensity minutes
+    add_intensity_minutes(datas, results_dict['intensity_min'])
+    # Sleep
+    add_sleep(date, user_id, datas, results_dict['sleep'], api, url)
 
-    if len(datas) > 0:
-        # Activity 
-        add_activity(datas, results_dict['activity'])
-        # Body battery
-        add_body_battery(datas, results_dict["body_battery"])
-        # Breath
-        add_breath(datas, results_dict['breath'])
-        # Calories 
-        add_calories(datas, results_dict['calories'])
-        # Cardio 
-        add_cardio(datas, results_dict['cardio'])
-        # Compute durations
-        add_durations(date, results_dict)
-        # Intensity minutes
-        add_intensity_minutes(datas, results_dict['intensity_min'])
-        # Sleep
-        add_sleep(date, user_id, datas, results_dict['sleep'], api, url)
-
-        # Spo2 
-        add_spo2(datas, results_dict['spo2'])
-        # Stress
-        add_stress(datas, results_dict['stress'])
+    # Spo2 
+    add_spo2(datas, results_dict['spo2'])
+    # Stress
+    add_stress(datas, results_dict['stress'])
 
     return copy.deepcopy(results_dict)
 
@@ -395,87 +393,81 @@ def convert_dict_to_df(signal_data, start_time) -> pd.DataFrame:
 def data_per_min(input_df):
     output_df = pd.DataFrame({"times" : [], "values" : []})
     first_minute = input_df["times"][0]
-    first_minute = pd.Timestamp(first_minute).round(freq='T') # round to minute
-
-    index_last_minut = input_df.tail(1).index[0]
-    last_minute = input_df["times"][index_last_minut]
-    last_minute = pd.Timestamp(last_minute).round(freq='T') # round to minute
- 
-    while first_minute < last_minute:
-        values_df = input_df.loc[input_df['times'] >= first_minute, ]
-        values = values_df.loc[values_df['times'] < first_minute + timedelta(minutes = 1), 'values']
-
-        aux_df = pd.DataFrame({"times" : first_minute, "values" : np.mean(values)}, index=[0])
-
-        output_df = pd.concat([output_df, aux_df ])
-        first_minute += + timedelta(minutes = 1)
-       
+    last_minute = input_df["times"][-1]
+    i = 0
+    while first_minute <= last_minute:
+        values = input_df.loc[input_df['times'] == first_minute, 'values']
+        output_df["times"][i] = first_minute
+        output_df["values"][i] = np.mean(values)
+        first_minute += + timedelta(min = 1)
+        i +=1
+    
     return output_df
 
 def initialize_dictionary_with_template() -> dict :  
     activity_dict = {
-        "distance" : "",
-        "goal" : "",
-        "intensity" : "",
-        "steps" : ""
+        "distance" : [],
+        "goal" : [],
+        "intensity" : [],
+        "steps" : []
     }
     body_battery_dict = {
-        "all_values" : "",
-        "highest" : "",
-        "lowest" : "",
+        "all_values" : [],
+        "highest" : [],
+        "lowest" : [],
     }
     breath_dict = {
-        "rate" : ""
+        "rate" : []
     }
     calories_dict = {
-        "active" : "", 
-        "resting" : "", 
-        "total" : "", 
+        "active" : [], 
+        "resting" : [], 
+        "total" : [], 
     }
     cardio_dict = {
-        "rate" : ""
+        "rate" : []
     }
     duration_dict = {
-        'intervals' : "", 
-        'collected' : "",
-        'day' : "", 
-        'night' : "",
-        'rest' : "",
-        'active' : ""
+        'intervals' : [], 
+        'collected' : [],
+        'day' : [], 
+        'night' : [],
+        'rest' : [],
+        'active' : []
     }
     intensity_min_dict = {
-        "moderate" : "",
-        "total" : "", 
-        "vigurous" : "",
+        "moderate" : [],
+        "total" : [], 
+        "vigurous" : [],
     }
     sleep_dict = {
-        "sleep_map" : "",
-        "awake" : "",
-        "deep" : "", 
-        "light" : "", 
-        "quality" : "",
-        "recorded_time" : "",
-        "rem" : "",
-        "score" : "",
-        "timestamp_end" : "",
+        "sleep_map" : [],
+        "awake" : [], 
+        "deep" : [], 
+        "light" : [], 
+        "quality" : [],
+        "recorded_time" : [],
+        "rem" : [], 
+        "score" : [],
+        "timestamp_end" : [],
         'percentage_deep' : 0,
         'percentage_light' : 0,
         'percentage_rem' : 0,
         'percentage_awake' : 0,
     }
     spo2_dict = {
-        "all_values" : "",
-        "averege" : "",
-        "lowest" : "",
+        "all_values" : [],
+        "averege" : [],
+        "lowest" : [],
     }
     stress_dict = {
-        "all_values" : "",
-        "high" : "", 
-        "low" : "", 
-        "medium" : "",
-        "recorded_time" : "",
-        "rest" : "", 
-        "score" : "",
+        "all_values" : [],
+        "high" : [], 
+        "low" : [], 
+        "medium" : [],
+        "recorded_time" : [],
+        "rest" : [], 
+        "score" : [],
     }
     
     dict_template = {
@@ -489,30 +481,30 @@ def initialize_dictionary_with_template() -> dict :
                     'sleep'        : copy.deepcopy(sleep_dict),
                     'spo2'         : copy.deepcopy(spo2_dict),
                     'stress'       : copy.deepcopy(stress_dict),
-                    'user_id'      : "",
+                    'user_id'      : [],
                     }
     return copy.deepcopy(dict_template)
 
 # %% ---------------------------- Test function ------------------------------ 
 # ----------------------------------------------------------------------------
-# from config import API_KEY_PREPROD, API_KEY_PROD, URL_GARMIN_PREPROD, URL_GARMIN_PROD
-# prod = False
-# # Michel
-# # user_id = "5Nwwut" 
-# # date = "2023-05-04" 
-# # Adriana
+from config import API_KEY_PREPROD, API_KEY_PROD, URL_GARMIN_PREPROD, URL_GARMIN_PROD
+prod = False
+# Michel
+# user_id = "5Nwwut" 
+# date = "2023-05-04" 
+# Adriana
 # user_id = "6o2Fzp"
-# date = "2023-06-01"
+# date = "2023-05-24"
 
-# # -- Ludo
-# # user_id = "4vk5VJ"
-# # date = "2023-05-25"
+# -- Ludo
+user_id = "4vk5VJ"
+date = "2023-05-25"
 
-# if prod == True :
-#     api = API_KEY_PROD
-#     url = URL_GARMIN_PROD
-# else :
-#     api = API_KEY_PREPROD
-#     url = URL_GARMIN_PREPROD
-# results_dict =  get_garmin_data(user_id, date, api, url)
+if prod == True :
+    api = API_KEY_PROD
+    url = URL_GARMIN_PROD
+else :
+    api = API_KEY_PREPROD
+    url = URL_GARMIN_PREPROD
+results_dict =  get_garmin_data(user_id, date, api, url)
 

automatic_reports/compute_common_for_html.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Last updated on 05/05/2023
+Last updated on 01/06/2023
 
 @author: aterman
 """
@@ -21,119 +21,131 @@ def get_common_indicators(cst_data:dict, garmin_data:dict) :
     common_indicators = initialize_dictionary_with_template()
 
     # ========================== Cardio dict ===============================
-     # ----------------------------- rate -------------------------------------
-    # Add rate high and max
-    values_df = common_data["cardio"]["rate"]["values"].dropna()
-    value = round(max(values_df))
-    common_indicators["cardio"]["rate_high"] = value
-    common_indicators["cardio"]["rate_max"] = value
-
-    # Add rate min
-    value = round(min(values_df))
-    common_indicators["cardio"]["rate_min"] = value
-
-    # Add rate mean
-    value = round(np.mean(values_df))
-    common_indicators["cardio"]["rate_mean"] = value
+    # ----------------------------- rate -------------------------------------
     
-    # Add rate resting 
-    mean_values = sliding_window(values_df, minutes = 30)
-    value = round(min(mean_values))
-    common_indicators["cardio"]["rate_resting"] = value
+    if len(common_data["cardio"]["rate"]) >0:
+    # Add rate high and max
+        values_df = common_data["cardio"]["rate"]["values"].dropna()
+        value = round(max(values_df))
+        common_indicators["cardio"]["rate_high"] = value
+        common_indicators["cardio"]["rate_max"] = value
+
+        # Add rate min
+        value = round(min(values_df))
+        common_indicators["cardio"]["rate_min"] = value
+
+        # Add rate mean
+        value = round(np.mean(values_df))
+        common_indicators["cardio"]["rate_mean"] = value
+        
+        # Add rate resting 
+        mean_values = sliding_window(values_df, minutes = 30)
+        value = round(min(mean_values))
+        common_indicators["cardio"]["rate_resting"] = value
 
     # --------------------------- rate var -----------------------------------
+    if len(common_data["cardio"]["rate_var"]) >0:
     # Add rate variability max and high
-    values_df = common_data["cardio"]["rate_var"]["values"].dropna()
-    value = round(max(values_df))
-    common_indicators["cardio"]["rate_var_high"] = value
-    common_indicators["cardio"]["rate_var_max"] = value
+        values_df = common_data["cardio"]["rate_var"]["values"].dropna()
+        value = round(max(values_df))
+        common_indicators["cardio"]["rate_var_high"] = value
+        common_indicators["cardio"]["rate_var_max"] = value
 
-    # Add rate variability min
-    value = round(min(values_df))
-    common_indicators["cardio"]["rate_var_min"] = value
+        # Add rate variability min
+        value = round(min(values_df))
+        common_indicators["cardio"]["rate_var_min"] = value
 
-    # Add rate variability mean
-    value = round(np.mean(values_df))
-    common_indicators["cardio"]["rate_var_mean"] = value
+        # Add rate variability mean
+        value = round(np.mean(values_df))
+        common_indicators["cardio"]["rate_var_mean"] = value
 
-    # Add rate variability resting
-    df_aux = common_data['cardio']['rate_var']
-    values = df_aux.loc[df_aux["activity_values"] <= ACTIVITY_THREASHOLD, 
-                        "values"].dropna().reset_index(drop=True)
-    value = round(np.mean(values))
-    common_indicators["cardio"]["rate_var_resting"] = value
+        # Add rate variability resting
+        df_aux = common_data['cardio']['rate_var']
+        values = df_aux.loc[df_aux["activity_values"] <= ACTIVITY_THREASHOLD, 
+                            "values"].dropna().reset_index(drop=True)
+        value = round(np.mean(values))
+        common_indicators["cardio"]["rate_var_resting"] = value
 
     # ========================== Breath dict =================================
     # ----------------------------- rate -------------------------------------
-    # Add rate high and max
-    values_df = common_data["breath"]["rate"]["values"].dropna()
-    value = round(max(values_df))
-    common_indicators["breath"]["rate_high"] = value
-    common_indicators["breath"]["rate_max"] = value
+    if len(common_data["breath"]["rate"]) >0:
+        # Add rate high and max
+        values_df = common_data["breath"]["rate"]["values"].dropna()
+        value = round(max(values_df))
+        common_indicators["breath"]["rate_high"] = value
+        common_indicators["breath"]["rate_max"] = value
 
-    # Add rate min
-    value = round(min(values_df))
-    common_indicators["breath"]["rate_min"] = value
+        # Add rate min
+        value = round(min(values_df))
+        common_indicators["breath"]["rate_min"] = value
 
-    # Add rate mean
-    value = round(np.mean(values_df))
-    common_indicators["breath"]["rate_mean"] = value
+        # Add rate mean
+        value = round(np.mean(values_df))
+        common_indicators["breath"]["rate_mean"] = value
 
-    # Add rate resting
-    mean_values = sliding_window(values_df, minutes = 30)
-    value = round(min(mean_values))
-    common_indicators["breath"]["rate_resting"] = value
+        # Add rate resting
+        mean_values = sliding_window(values_df, minutes = 30)
+        value = round(min(mean_values))
+        common_indicators["breath"]["rate_resting"] = value
 
     # --------------------------- rate var -----------------------------------
-    # Add rate variability high and max
-    values_df = common_data["breath"]["rate_var"]["values"].dropna()
-    common_indicators["breath"]["rate_var_max"] = round(max(values_df))
-    common_indicators["breath"]["rate_var_high"] = round(max(values_df))
+    if len(common_data["breath"]["rate_var"]) >0:
+        # Add rate variability high and max
+        values_df = common_data["breath"]["rate_var"]["values"].dropna()
+        common_indicators["breath"]["rate_var_max"] = round(max(values_df))
+        common_indicators["breath"]["rate_var_high"] = round(max(values_df))
 
-    # Add rate variability min
-    common_indicators["breath"]["rate_var_min"] = round(min(values_df))
+        # Add rate variability min
+        common_indicators["breath"]["rate_var_min"] = round(min(values_df))
 
-    # Add rate variability mean
-    common_indicators["breath"]["rate_var_mean"] = round(np.mean(values_df))
+        # Add rate variability mean
+        common_indicators["breath"]["rate_var_mean"] = round(np.mean(values_df))
 
-    # Add rate variability resting
-    df_aux = common_data['breath']['rate_var']
-    values = df_aux.loc[df_aux["activity_values"] <= ACTIVITY_THREASHOLD, 
-                        "values"].dropna().reset_index(drop=True)
-    value = round(np.mean(values))
-    common_indicators["breath"]["rate_var_resting"] = value
+        # Add rate variability resting
+        df_aux = common_data['breath']['rate_var']
+        values = df_aux.loc[df_aux["activity_values"] <= ACTIVITY_THREASHOLD, 
+                            "values"].dropna().reset_index(drop=True)
+        value = round(np.mean(values))
+        common_indicators["breath"]["rate_var_resting"] = value
 
     # ------------------------- inspi expi -----------------------------------
-    values_df = common_data["breath"]["inspi_expi"]["values"].dropna()
+    if len(common_data["breath"]["inspi_expi"]) >0:
+        values_df = common_data["breath"]["inspi_expi"]["values"].dropna()
 
-    # Add rate variability high and max
-    common_indicators["breath"]["inspi_expi_max"] =  round(max(values_df))
-    
-    # Add rate variability min
-    common_indicators["breath"]["inspi_expi_min"] = round(min(values_df))
+        # Add rate variability high and max
+        common_indicators["breath"]["inspi_expi_max"] =  round(max(values_df))
+        
+        # Add rate variability min
+        common_indicators["breath"]["inspi_expi_min"] = round(min(values_df))
 
-    # Add rate variability mean
-    common_indicators["breath"]["inspi_expi_mean"] = round(np.mean(values_df))
+        # Add rate variability mean
+        common_indicators["breath"]["inspi_expi_mean"] = round(np.mean(values_df))
 
     # ========================== Activity dict ===============================
     # dictionary with data used to plot steps graph 
     steps_dict_for_plot = {
-        "total_steps" : None,
-        "goal" : None
+        "total_steps" : "",
+        "goal" : ""
     }
-    # Steps
-    value = round(sum(common_data["activity"]["steps"]["values"].dropna()))
-    common_indicators["activity"]["steps"] = value
-    steps_dict_for_plot["total_steps"] = value
 
-    # Goal
-    value = garmin_data["activity"]["goal"]
-    common_indicators["activity"]["goal"] = value
-    steps_dict_for_plot["goal"] = value
+    if len(common_data["activity"]["steps"]) >0:
+        # Steps
+        value = round(sum(common_data["activity"]["steps"]["values"].dropna()))
+        common_indicators["activity"]["steps"] = value
+        steps_dict_for_plot["total_steps"] = value
 
-    # Distance
-    value = round(sum(common_data["activity"]["distance"]["values"].dropna()))
-    common_indicators["activity"]["distance"] = value
+        # Goal        
+        if garmin_data["activity"]["goal"] == "":
+            value = 3000
+        else :
+            value = garmin_data["activity"]["goal"]
+
+        common_indicators["activity"]["goal"] = value
+        steps_dict_for_plot["goal"] = value
+
+        # Distance
+        value = round(sum(common_data["activity"]["distance"]["values"].dropna()))
+        common_indicators["activity"]["distance"] = value
 
     return common_data, common_indicators, steps_dict_for_plot
 
@@ -142,15 +154,15 @@ def get_common_indicators(cst_data:dict, garmin_data:dict) :
 def  combine_data(cst_data, garmin_data):
     
     cardio_dict = {
-        "rate" : None,
-        "rate_var" : None
+        "rate" : "",
+        "rate_var" : "",
         }
     breath_dict = {
-        "rate" : None,
+        "rate" : "",
         }
     activity_dict = {
-        "steps" : None,
-        "distance" : None,
+        "steps" : "",
+        "distance" : "",
         }
     
     common_data = {  
@@ -162,10 +174,15 @@ def  combine_data(cst_data, garmin_data):
     # --- Cardio ---
     # Rate 
     garmin_df = garmin_data["cardio"]["rate"]
-    cst_df = cst_data["cardio"]["rate"][["times", "values"]]
+    cst_df = cst_data["cardio"]["rate"]
 
-    common_data["cardio"]["rate"] = merge_cst_and_garmin_data(cst_df, garmin_df)
-    
+    if len(garmin_df)>0 and len(cst_df)>0:
+        common_data["cardio"]["rate"] = merge_cst_and_garmin_data(cst_df[["times", "values"]], garmin_df)
+    elif len(garmin_df) == 0 and  len(cst_df)>0:
+        common_data["cardio"]["rate"] = cst_df[["times", "values"]]
+    elif len(garmin_df) > 0 and  len(cst_df) == 0:
+        common_data["cardio"]["rate"] = garmin_df
+
     # Rate variability
     cst_df = cst_data["cardio"]["rate_var"]
     common_data["cardio"]["rate_var"] = cst_df
@@ -173,8 +190,14 @@ def  combine_data(cst_data, garmin_data):
     # --- Breath ---
     # Rate 
     garmin_df = garmin_data["breath"]["rate"]
-    cst_df    = cst_data["breath"]["rate"][["times", "values"]]
-    common_data["breath"]["rate"] = merge_cst_and_garmin_data(cst_df, garmin_df)
+    cst_df    = cst_data["breath"]["rate"]
+    
+    if len(garmin_df)>0 and len(cst_df)>0:
+        common_data["breath"]["rate"] = merge_cst_and_garmin_data(cst_df[["times", "values"]], garmin_df)
+    elif len(garmin_df) == 0 and  len(cst_df)>0:
+        common_data["breath"]["rate"] = cst_df[["times", "values"]]
+    elif len(garmin_df) > 0 and  len(cst_df) == 0:
+        common_data["breath"]["rate"] = garmin_df
 
     # Rate variability
     cst_df = cst_data["breath"]["rate_var"]
@@ -186,54 +209,78 @@ def  combine_data(cst_data, garmin_data):
     
     # --- Activity ---
     # Steps 
-    garmin_df = garmin_data["activity"]["intensity"][["times", "steps"]]
-    garmin_df = garmin_df.rename(columns={"steps": "values"})
     cst_df    = cst_data["activity"]["steps"]
-    common_data["activity"]["steps"] = merge_cst_and_garmin_data(garmin_df, cst_df)
+    garmin_df = garmin_data["activity"]["intensity"]
+    
+    if len(garmin_df)>0 and len(cst_df)>0:
+        garmin_df = garmin_df[["times", "steps"]]
+        garmin_df = garmin_df.rename(columns={"steps": "values"})
+        
+        common_data["activity"]["steps"] = merge_cst_and_garmin_data(garmin_df, cst_df)
+    
+    elif len(garmin_df) == 0 and  len(cst_df)>0:
+        common_data["activity"]["steps"] = cst_df
+
+    elif len(garmin_df) > 0 and  len(cst_df) == 0:
+        garmin_df = garmin_df[["times", "steps"]]
+        garmin_df = garmin_df.rename(columns={"steps": "values"})
+        common_data["activity"]["steps"] = garmin_df
 
     # Distance 
-    garmin_df = garmin_data["activity"]["intensity"][["times", "distance"]]
-    garmin_df.rename(columns = {'distance':'values'}, inplace = True)
     cst_df    = cst_data["activity"]["distance"]
-    common_data["activity"]["distance"] = merge_cst_and_garmin_data(garmin_df, cst_df)
+    garmin_df = garmin_data["activity"]["intensity"]
     
+    if len(garmin_df)>0 and len(cst_df)>0:
+        garmin_df = garmin_df[["times", "distance"]]
+        garmin_df.rename(columns = {'distance':'values'}, inplace = True)
+
+        common_data["activity"]["distance"] = merge_cst_and_garmin_data(garmin_df, cst_df)
+    
+    elif len(garmin_df) == 0 and  len(cst_df)>0:
+        common_data["activity"]["distance"] = cst_df
+
+    elif len(garmin_df) > 0 and  len(cst_df) == 0:
+        garmin_df = garmin_df[["times", "distance"]]
+        garmin_df = garmin_df.rename(columns={"distance": "values"})
+        common_data["activity"]["distance"] = garmin_df
+
     return copy.deepcopy(common_data)
 
 def initialize_dictionary_with_template() -> dict :
     cardio_dict = {
-        "rate_high"        : None,
-        "rate_resting"     : None,
-        "rate_min"         : None,
-        "rate_max"         : None,
-        "rate_mean"        : None,
+        "rate_high"        : "",
+        "rate_resting"     : "",
+        "rate_min"         : "",
+        "rate_max"         : "",
+        "rate_mean"        : "",
 
-        "rate_var_resting" : None,
-        "rate_var_high"    : None,
-        "rate_var_min"     : None,
-        "rate_var_max"     : None,
-        "rate_var_mean"    : None,
+        "rate_var_resting" : "",
+        "rate_var_high"    : "",
+        "rate_var_min"     : "",
+        "rate_var_max"     : "",
+        "rate_var_mean"    : "",
     } 
     breath_dict = {
-        "rate_high"        : None,
-        "rate_max"         : None,
-        "rate_min"         : None,
-        "rate_mean"        : None,
-        "rate_resting"     : None,
+        "rate_high"        : "",
+        "rate_max"         : "",
+        "rate_min"         : "",
+        "rate_mean"        : "",
+        "rate_resting"     : "",
 
-        "rate_var_high"    : None,
-        "rate_var_max"     : None,
-        "rate_var_min"     : None,
-        "rate_var_mean"    : None,
-        "rate_var_resting" : None,
+        "rate_var_high"    : "",
+        "rate_var_max"     : "",
+        "rate_var_min"     : "",
+        "rate_var_mean"    : "",
+        "rate_var_resting" : "",
         
-        "inspi_expi_max"  : None,
-        "inspi_expi_min"  : None,
-        "inspi_expi_mean" : None,
+        "inspi_expi_max"  : "",
+        "inspi_expi_min"  : "",
+        "inspi_expi_mean" : "",
     } 
     activity_dict = {
-        "steps"            : None,
-        "distance"         : None,
-        "goal"             : None,
+        "steps"            : "",
+        "distance"         : "",
+        "goal"             : "",
     }
     dict_template = {
                     'cardio'   : copy.deepcopy(cardio_dict),

automatic_reports/plot_images.py
@@ -28,7 +28,10 @@ def plot_images(garmin_data, steps_dict,
 
 # --- Steps ---
 def plot_steps(steps_dict, path_save):
-    steps_score = round(steps_dict["total_steps"]/steps_dict["goal"]*100)
+    if steps_dict["goal"].isdigit() > 0:
+        steps_score = round(steps_dict["total_steps"]/steps_dict["goal"]*100)
+    else : 
+        steps_score = 0
     
     plt.figure(figsize=(5,5))
     if (steps_score < 100):
@@ -59,52 +62,54 @@ def plot_spo2(spo2_dict, path_save):
     ORANGE = "#F77517"
     RED = "#CE4A14"
 
-    if spo2_dict["all_values"] is not None:
+    if spo2_dict["averege"].isdigit():
         averege_score = spo2_dict["averege"]
         lowest_score = spo2_dict["lowest"]
+    else: 
+        averege_score = 30
+        lowest_score = 30
+
+    size_of_groups=[4,23,23,23,23,4]
+
+    plt.figure(figsize=(5,5))
+    plt.pie(size_of_groups, 
+            colors=["white", GREEN, YELLOW, ORANGE, RED, "white"], 
+            startangle=270)
+    
+    my_circle=plt.Circle( (0,0), 0.9, color="white")
+    p=plt.gcf()
+    p.gca().add_artist(my_circle)
+
+    plt.text(0, 0.30, 'Averege Spo2', fontsize=18, color=GREY,  horizontalalignment = "center")
+    plt.text(0, 0, (str(averege_score) + '%'), fontsize=30, color = BLUE, horizontalalignment = "center")
+    plt.text(0, -0.40, 'Lowest', fontsize=18, color=GREY,  horizontalalignment = "center")
+    plt.text(0, -0.65, (str(lowest_score) + '%'), fontsize=20, color=BLUE,  horizontalalignment = "center")
+
+    # setting the axes projection as polar
+    plt.axes(projection = 'polar')
+    radius = 0.75
+
+    score_reshape = ((100 - 0)/(100-60)) * (averege_score - 60)
+    deg = (270-(4/100*360)) - score_reshape/100*(92/100*360)
+    rad = np.deg2rad(deg)
+
+    if averege_score < 70:
+        color_spo2_score=RED
+    elif 70 <= averege_score < 80:
+        color_spo2_score=ORANGE
+    elif 80 <= averege_score < 90:
+        color_spo2_score=YELLOW
+    elif 90 <= averege_score <= 100:
+        color_spo2_score=GREEN
         
-         
-        size_of_groups=[4,23,23,23,23,4]
-    
-        plt.figure(figsize=(5,5))
-        plt.pie(size_of_groups, 
-                colors=["white", GREEN, YELLOW, ORANGE, RED, "white"], 
-                startangle=270)
-        
-        my_circle=plt.Circle( (0,0), 0.9, color="white")
-        p=plt.gcf()
-        p.gca().add_artist(my_circle)
-    
-        plt.text(0, 0.30, 'Averege Spo2', fontsize=18, color=GREY,  horizontalalignment = "center")
-        plt.text(0, 0, (str(averege_score) + '%'), fontsize=30, color = BLUE, horizontalalignment = "center")
-        plt.text(0, -0.40, 'Lowest', fontsize=18, color=GREY,  horizontalalignment = "center")
-        plt.text(0, -0.65, (str(lowest_score) + '%'), fontsize=20, color=BLUE,  horizontalalignment = "center")
-    
-        # setting the axes projection as polar
-        plt.axes(projection = 'polar')
-        radius = 0.75
-    
-        score_reshape = ((100 - 0)/(100-60)) * (averege_score - 60)
-        deg = (270-(4/100*360)) - score_reshape/100*(92/100*360)
-        rad = np.deg2rad(deg)
-    
-        if averege_score < 70:
-            color_spo2_score=RED
-        elif 70 <= averege_score < 80:
-            color_spo2_score=ORANGE
-        elif 80 <= averege_score < 90:
-            color_spo2_score=YELLOW
-        elif 90 <= averege_score <= 100:
-            color_spo2_score=GREEN
-            
-        plt.polar(rad, radius, '.', markersize=60, color=color_spo2_score)
-        plt.polar(rad, 1, '.', color = "white")
-        plt.axis('off')
-        plt.savefig(path_save + "/spo2.png", transparent=True)
+    plt.polar(rad, radius, '.', markersize=60, color=color_spo2_score)
+    plt.polar(rad, 1, '.', color = "white")
+    plt.axis('off')
+    plt.savefig(path_save + "/spo2.png", transparent=True)
 
 # --- Sleep ---
 def plot_sleep(sleep_dict, path_save):
-    if sleep_dict["recorded_time"] > 0:
+    if sleep_dict["recorded_time"].isdigit():
         score   = sleep_dict["score"]
         quality = sleep_dict["quality"]
         deep    = sleep_dict["deep"]/sleep_dict["recorded_time"]
@@ -131,12 +136,21 @@ def plot_sleep(sleep_dict, path_save):
     
 # --- Stress ---
 def plot_stress(stress_dict, path_save):
-    stress_score = stress_dict["score"]
+
     recorded_time = stress_dict["recorded_time"]
-    rest = stress_dict["rest"]/recorded_time
-    low = stress_dict["low"]/recorded_time
-    medium = stress_dict["medium"]/recorded_time
-    high = stress_dict["high"]/recorded_time
+    if recorded_time.isdigit():
+        stress_score = stress_dict["score"]
+        rest = stress_dict["rest"]/recorded_time
+        low = stress_dict["low"]/recorded_time
+        medium = stress_dict["medium"]/recorded_time
+        high = stress_dict["high"]/recorded_time
+
+    else : 
+        stress_score = 0
+        rest = 25
+        low = 25
+        medium = 25
+        high = 25
 
     size_of_groups=[rest, low, medium, high]
 

automatic_reports/useful_functions.py
@@ -78,6 +78,29 @@ def timedelta_formatter(time_delta, convert = False):    # defining the function
     msg = "{}h, {}min".format(hour_count,minute_count)
     return msg 
 
+def unwrap_ratio_inspi_expi(values):
+    """ Unwrap values from list
+
+     Parameter
+    ----------
+    values:     Values to unwrap
+
+    Returns
+    ----------
+    new_values: unwrapped values
+
+    # """
+
+    new_values = []
+    for value in values:     
+        if value is not None and type(value)==float:            
+            new_values.append(value)
+        else:
+             if value is not None:
+                 new_values.extend(value) 
+
+    return np.array(new_values)
+
 def unwrap(values):
     """ Unwrap values from list
 
@@ -89,7 +112,7 @@ def unwrap(values):
     ----------
     new_values: unwrapped values
 
-    """
+    # """
     if not is_list_of_list(values):
         return values
 

template/chart.py
@@ -21,6 +21,7 @@ def temperature_mean():
 
     fig = go.Figure()
 
+    
     values_df = get_temperature()['values']
     if len(values_df) > 0:
         x = values_df["times"]
@@ -48,49 +49,51 @@ def sleep():
     
     translate = st.session_state.translate
         
-    map_sleep = get_sleep()['values']
-    awake = map_sleep['awake']
-    rem = map_sleep['rem']
-    light = map_sleep['light']
-    deep = map_sleep['deep']
-
     fig = go.Figure()
-    
-    # Add 2 point on the graph for refference
-    date = st.session_state.date
-    date_1 = datetime.datetime.strptime(date, "%Y-%m-%d")
-    date_2 = date_1 + datetime.timedelta(hours = 10)
-    fig.add_trace(go.Scatter(x=[date_1, date_2], y=[2, 2], marker_color="white"))
 
-    for interval in awake:
-        xend = datetime.datetime.fromtimestamp(interval["endTimeInSeconds"])
-        xstart = datetime.datetime.fromtimestamp(interval["startTimeInSeconds"])
-        fig.add_shape(type="rect", line_width=0,
-                        x0=xstart, y0=0, x1=xend, y1=8,
-                        fillcolor=constant.COLORS()['sleep_awake'])
+    map_sleep = get_sleep()['values']
+    if isinstance(map_sleep, str) == False:
+        awake = map_sleep['awake']
+        rem = map_sleep['rem']
+        light = map_sleep['light']
+        deep = map_sleep['deep']
+        
+        # Add 2 point on the graph for refference
+        date = st.session_state.date
+        date_1 = datetime.datetime.strptime(date, "%Y-%m-%d")
+        date_2 = date_1 + datetime.timedelta(hours = 10)
+        fig.add_trace(go.Scatter(x=[date_1, date_2], y=[2, 2], marker_color="white"))
 
-    for interval in rem:
-        xend = datetime.datetime.fromtimestamp(interval["endTimeInSeconds"])
-        xstart = datetime.datetime.fromtimestamp(interval["startTimeInSeconds"])
-        fig.add_shape(type="rect", line_width=0, 
-                        x0=xstart, y0=0, x1=xend, y1=6,
-                        fillcolor=constant.COLORS()['sleep_rem'])
+        for interval in awake:
+            xend = datetime.datetime.fromtimestamp(interval["endTimeInSeconds"])
+            xstart = datetime.datetime.fromtimestamp(interval["startTimeInSeconds"])
+            fig.add_shape(type="rect", line_width=0,
+                            x0=xstart, y0=0, x1=xend, y1=8,
+                            fillcolor=constant.COLORS()['sleep_awake'])
 
-    for interval in light:
-        xend = datetime.datetime.fromtimestamp(interval["endTimeInSeconds"])
-        xstart = datetime.datetime.fromtimestamp(interval["startTimeInSeconds"])
-        fig.add_shape(type="rect", line_width=0, 
-                        x0=xstart, y0=0, x1=xend, y1=4,
-                        fillcolor=constant.COLORS()['sleep_light'])
+        for interval in rem:
+            xend = datetime.datetime.fromtimestamp(interval["endTimeInSeconds"])
+            xstart = datetime.datetime.fromtimestamp(interval["startTimeInSeconds"])
+            fig.add_shape(type="rect", line_width=0, 
+                            x0=xstart, y0=0, x1=xend, y1=6,
+                            fillcolor=constant.COLORS()['sleep_rem'])
 
-    for interval in deep:
-        xend = datetime.datetime.fromtimestamp(interval["endTimeInSeconds"])
-        xstart = datetime.datetime.fromtimestamp(interval["startTimeInSeconds"])
-        fig.add_shape(type="rect", line_width=0, 
-                        x0=xstart, y0=0, x1=xend, y1=2,
-                        fillcolor=constant.COLORS()['sleep_deep'])
+        for interval in light:
+            xend = datetime.datetime.fromtimestamp(interval["endTimeInSeconds"])
+            xstart = datetime.datetime.fromtimestamp(interval["startTimeInSeconds"])
+            fig.add_shape(type="rect", line_width=0, 
+                            x0=xstart, y0=0, x1=xend, y1=4,
+                            fillcolor=constant.COLORS()['sleep_light'])
 
-    fig.update_shapes(dict(xref='x', yref='y'))
+        for interval in deep:
+            xend = datetime.datetime.fromtimestamp(interval["endTimeInSeconds"])
+            xstart = datetime.datetime.fromtimestamp(interval["startTimeInSeconds"])
+            fig.add_shape(type="rect", line_width=0, 
+                            x0=xstart, y0=0, x1=xend, y1=2,
+                            fillcolor=constant.COLORS()['sleep_deep'])
+
+        fig.update_shapes(dict(xref='x', yref='y'))
+
     fig.update_layout(xaxis_title=translate["times"],
                     font=dict(size=14,),
                     yaxis = dict(
@@ -213,7 +216,6 @@ def pulseox():
     return fig
 
 def stress():
-    
     translate = st.session_state.translate
     thr_low     = 25
     thr_medium  = 50
@@ -222,70 +224,70 @@ def stress():
     fig = go.Figure()
 
     values_df = get_stress()['values']
-    values_df = values_df.loc[values_df["values"] > 0].dropna().reset_index(drop=True)
-    if len(values_df) > 0:
-        x = values_df["times"]
-        y = values_df["values"]
-    
-        idx = np.where(y < thr_low)
-        if len(idx[0]) > 0:
-            idx = idx[0]
-        else:
-            idx = []
-        x_rest = x[idx]
-        y_rest = y[idx]
+    if isinstance(values_df, str) == False: 
+        values_df = values_df.loc[values_df["values"] > 0].dropna().reset_index(drop=True)
+        if len(values_df) > 0:
+            x = values_df["times"]
+            y = values_df["values"]
         
-        idx = np.where((y > thr_low) & (y <= thr_medium))
-        if len(idx[0]) > 0:
-            idx = idx[0]
-        else:
-            idx = []
-        x_low = x[idx]
-        y_low = y[idx]
-        
-        idx = np.where((y > thr_medium) & (y <= thr_high))
-        if len(idx[0]) > 0:
-            idx = idx[0]
-        else:
-            idx = []
-        x_medium = x[idx]
-        y_medium = y[idx]
-        
-        idx = np.where(y > thr_high)
-        if len(idx[0]) > 0:
-            idx = idx[0]
-        else:
-            idx = []
-        x_high = x[idx]
-        y_high = y[idx]
-        
-    
-        fig.add_trace(go.Bar(x=x_rest, 
-                            y=y_rest,
-                            marker_color=constant.COLORS()['stress_rest'],
-                            name="Rest"))
-        fig.add_trace(go.Bar(x=x_low, 
-                            y=y_low,
-                            marker_color=constant.COLORS()['stress_low'],
-                            name="Low"))
-        fig.add_trace(go.Bar(x=x_medium, 
-                            y=y_medium,
-                            marker_color=constant.COLORS()['stress_medium'],
-                            name="Medium"))
-        fig.add_trace(go.Bar(x=x_high, 
-                            y=y_high,
-                            marker_color=constant.COLORS()['stress_high'],
-                            name="High"))
-        
+            idx = np.where(y < thr_low)
+            if len(idx[0]) > 0:
+                idx = idx[0]
+            else:
+                idx = []
+            x_rest = x[idx]
+            y_rest = y[idx]
+            
+            idx = np.where((y > thr_low) & (y <= thr_medium))
+            if len(idx[0]) > 0:
+                idx = idx[0]
+            else:
+                idx = []
+            x_low = x[idx]
+            y_low = y[idx]
+            
+            idx = np.where((y > thr_medium) & (y <= thr_high))
+            if len(idx[0]) > 0:
+                idx = idx[0]
+            else:
+                idx = []
+            x_medium = x[idx]
+            y_medium = y[idx]
+            
+            idx = np.where(y > thr_high)
+            if len(idx[0]) > 0:
+                idx = idx[0]
+            else:
+                idx = []
+            x_high = x[idx]
+            y_high = y[idx]
+                    
+            fig.add_trace(go.Bar(x=x_rest, 
+                                y=y_rest,
+                                marker_color=constant.COLORS()['stress_rest'],
+                                name="Rest"))
+            fig.add_trace(go.Bar(x=x_low, 
+                                y=y_low,
+                                marker_color=constant.COLORS()['stress_low'],
+                                name="Low"))
+            fig.add_trace(go.Bar(x=x_medium, 
+                                y=y_medium,
+                                marker_color=constant.COLORS()['stress_medium'],
+                                name="Medium"))
+            fig.add_trace(go.Bar(x=x_high, 
+                                y=y_high,
+                                marker_color=constant.COLORS()['stress_high'],
+                                name="High"))
+            
     fig.update_layout(xaxis_title=translate["times"],
-                      yaxis_title="Stress Scores",
-                      font=dict(size=14,),
-                      height=300, 
-                      template="plotly_white",
-                      paper_bgcolor=BGCOLOR, plot_bgcolor=BGCOLOR,
-                      title="Stress scores",
-                      showlegend=False,
-                      )
+                    yaxis_title="Stress Scores",
+                    font=dict(size=14,),
+                    height=300, 
+                    template="plotly_white",
+                    paper_bgcolor=BGCOLOR, plot_bgcolor=BGCOLOR,
+                    title="Stress scores",
+                    showlegend=False,
+                    )
     
     return fig
 
@@ -581,19 +583,20 @@ def sleep_donut():
     sleep       = data.get_sleep()
     
     recoded_time   = sleep["duration"]
-    if recoded_time > 1:
+    if isinstance(recoded_time, str) == False:
         deep    = sleep["percentage_deep"]
         light   = sleep["percentage_light"]
         rem     = sleep["percentage_rem"]
         awake   = sleep["percentage_awake"]
+        score   = sleep["score"]
+        quality = sleep["quality"]
     else:
         deep    = 25
         light   = 25
         rem     = 25
         awake   = 25
-
-    score   = sleep["score"]
-    quality = sleep["quality"]
+        score   = ""
+        quality = "no data"
     
     plt.rcParams['figure.facecolor'] = "white" #cycler(color="#F0F2F6")
     size_of_groups=[deep, light, rem, awake]
@@ -613,7 +616,7 @@ def sleep_donut():
     my_circle=plt.Circle( (0,0), 0.8, color="white")
     p=plt.gcf()
     p.gca().add_artist(my_circle)
-    if sleep["score"] == "":
+    if score == "":
         plt.text(0, -1.5, translate["no_data"], fontsize=40, color=constant.COLORS()["text"],
                  horizontalalignment='center',verticalalignment='center')
         